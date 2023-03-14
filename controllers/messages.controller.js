bf5f41241c8ab2cec47cc3ed8bd0a36af6072b70
Maël Raingeard <rgdmael@gmail.com>
2023-03-14 07:44:45 +0100

    fix: message formated & route message

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,50 +1,51 @@
 const db = require("../models");
-const config = require("../config/auth.config");
 const { QueryTypes } = require('sequelize');
-
-const sqlFindAll = `
-SELECT m.* 
-	FROM user_roles ur
-    JOIN channel_roles cr on cr.roleId = ur.roleId
-    JOIN channels c on c.id = cr.channelId
-    JOIN messages m on m.idChannel = c.id
-    WHERE ur.userId = $1
-    
- UNION 
-SELECT m.*
-    FROM messages m
-    WHERE m.idChannel IS NULL
-    AND (m.idSender = $1 OR m.idRecipient = $1)
-`;
 
 const messageFormated = (message) =>  {
     return {
         id: message.id,
         content: message.content,
         image: message.image,
-        isSeen: message.isSeen,
-        senderId: message.idSender,
+        isSeen: message.is_seen,
+        senderId: message.senderId,
         createdAt: message.createdAt,
         updatedAt: message.updatedAt
 }};
 
+const sqlFindAll = `
+    SELECT m.* 
+    FROM user_roles ur
+    JOIN channel_roles cr on cr.roleId = ur.roleId
+    JOIN channels c on c.id = cr.channelId
+    JOIN messages m on m.channelId = c.id
+    WHERE ur.userId = $1 AND c.organisationId = $2
+
+UNION 
+    SELECT m.*
+    FROM messages m
+    JOIN users u1
+    WHERE m.channelId IS NULL
+    AND m.organisationId = '94d0356e-0562-4987-8837-ad9719617802'
+    AND (m.senderId = $1 OR m.recipientId = $1)
+`;
+
 exports.findAll = async (req, res) => {
     const messages = await db.sequelize.query(sqlFindAll, {
-        bind: [req.userId],
+        bind: [req.userId, req.params.organisationId],
         type: QueryTypes.SELECT
     });
 
     const messagesMapped = messages.reduce((acc, message) => {
-        const type = message.idChannel ? 'channels' : 'privates';
-        const index = acc[type].findIndex(channel => channel.id === (message.idChannel ?? message.idSender));
+        const type = message.channelId ? 'channels' : 'privates';
+        const index = acc[type].findIndex(channel => channel.id === (message.channelId ?? message.senderId));
             
         if(index !== -1) {
-            acc[type][index].messages.push(messageFormated(message));
+            acc[type][index].messages.push(message);
         }
         else {
             acc[type].push({
-                id: message.idChannel ?? message.idSender,
-                messages: [messageFormated(message)]
+                id: message.channelId ?? message.senderId,
+                messages: [message]
             });
         }
         return acc;
@@ -52,4 +53,4 @@ exports.findAll = async (req, res) => {
     },{channels: [], privates: []});
     
     return res.send(messagesMapped); 
-}
+};
--- routes/messages.route.js
+++ routes/messages.route.js
@@ -2,8 +2,5 @@ const controller = require("../controllers/messages.controller");
 const authJwt = require("../middlewares/authJwt");
 
 module.exports = function(app) {
-    console.log('authJwt.verifyToken :>> ', authJwt.verifyToken);
-    app.use("/api/messages", authJwt.verifyToken);
-
-    app.get("/api/messages", controller.findAll);
+    app.get("/:organisationId/messages", controller.findAll);
 };
